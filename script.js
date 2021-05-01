f657f6b77f521818855f00e0ad509633258842ba
arwin-new-world <83156405+arwin-new-world@users.noreply.github.com>
2021-05-01 20:22:29 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 const canvas=document.getElementById("canvas");
 const c=canvas.getContext("2d");
+var xhr = new XMLHttpRequest();
+xhr.open("POST","https://arwin-new-world.github.io/chat/");
+xhr.setRequestHeader('content-type', 'application/x-www-form-urlencoded;charset=UTF-8');
